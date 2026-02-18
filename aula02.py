fcec7620ea39971cc37613726d8345bfbfde53d9
Kauã Gabriel <machadokaua546@gmail.com>
2026-02-18 12:44:13 -0300

    primeira aula de progamação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 67 # variável int
+numero_quebrado = 3.37 # variável float
+lindofo_foi_embora = True # variável booleana
+texto = "lindofo vem amanha" # variável de texto
+
+
+print(lindofo)
+print(numero_quebrado)
+print(lindofo_foi_embora)
+print(texto)
