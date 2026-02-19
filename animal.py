a056eab47ef485bcebc81db92f7a60400f399a75
kaua <machadokaua546@gmail.com>
2026-02-19 09:51:21 -0300

    animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "cachorro" # variável de texto
+idade = 3 # variável de int
+peso = 9.01 # variável de float 
+sexo = "macho" # variáel de texto
+
+
+print(nome) 
+print(idade)
+print(peso)
+print(sexo)
