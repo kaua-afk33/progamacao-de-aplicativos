9d7a34e195b357ff57ea9b8d35b91043cdbbea0f
kaua <machadokaua546@gmail.com>
2026-03-11 11:59:03 -0300

    atividade

--- peso.py
+++ peso.py
@@ -2,8 +2,8 @@ peso = input("digite o peso")
 codigo = (input("digite o codigo"))
 
 if peso >"50":
-
-    if peso < "5" and codigo == "123" % 10 == 0:
+    status = "carga pesada"
+elif peso < "5" and codigo == "123" % 10 == 0:
       print(f"aprovado pacote {codigo} status de entrega")
 else:
     print("negado")
--- sorteio.py
+++ sorteio.py
@@ -1,8 +1,7 @@
-id = input("digite o id")
+user_id = int(input("digite o seu id"))
 valor_compra = float(input("digite o valor da compra"))
 
-if id % 2 == 0 and valor_compra >= "500.00":
-    print("parabens, {id} voce ganhou cupom para sua compra de R$ {valor}")
-
+if user_id % 2 == 0 and valor_compra > 500.00:
+    print(f"Parabéns, usuário {user_id}! Você ganhou um cupom para sua compra de R$ {valor_compra}.")
 else:
-    print("brigado pela compra {id} continue acompanhando as nossas promocoes")
+    print(f"Obrigado pela compra, usuário {user_id}. Continue acompanhando nossas promoções!")
