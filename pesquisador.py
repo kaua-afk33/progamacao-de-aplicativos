c8eb05b9dd1d462480758079e4f69c46947f27c8
kaua <machadokaua546@gmail.com>
2026-03-23 12:35:20 -0300

    atividade

---
+++ pesquisador.py
@@ -0,0 +1,26 @@
+
+autorizados = ["Alice", "Bob", "Carlos"]
+print(f"Lista atual: {autorizados}")
+
+
+nome = input("Digite o nome do pesquisador: ")
+
+
+if nome in autorizados:
+   
+    indice = autorizados.index(nome)
+    print(f"Acesso Permitido! O pesquisador {nome} está na posição {indice}.")
+    
+  
+    decisao = input(f"Deseja remover {nome} da lista? (Sim/Não): ")
+    if decisao.lower() == "sim":
+        autorizados.remove(nome)
+        print(f"Pesquisador {nome} removido.")
+        print(f"Lista atualizada: {autorizados}")
+    else:
+        print("Operação cancelada. A lista permanece a mesma.")
+
+
+
+    
+
