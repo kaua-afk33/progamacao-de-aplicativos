3170d93825db7d710119bc48fbff7e6f6dae66bb
kaua <machadokaua546@gmail.com>
2026-03-26 10:00:12 -0300

    atividade

---
+++ fila.py
@@ -0,0 +1,6 @@
+lista = ["Ana" , "Cristiano" , "Kleber" , "Polar" , "Thiago"]
+
+while lista == [0]:
+    lista.pop[0]
+    print("{lista} foi atendidos")
+    
--- filtro.py
+++ filtro.py
@@ -1,6 +1,5 @@
 nomes = ["Ana", "Carlos", "Beatriz", "Luiz", "Fernanda", "João", "Gabriela"]
 
 for nome in nomes:
-as
     if len(nome) > 5:
         print(nome)
