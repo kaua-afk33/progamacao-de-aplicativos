6bd86e3e7c4812b13570cdade0761abed8cec4a2
unknown <machadokaua546@gmail.com>
2026-03-31 11:53:38 -0300

    atividade

---
+++ media.py
@@ -0,0 +1,7 @@
+media1 = float(input("digite a sua media primeiro trimestre"))
+media2 = float(input("digite a sua nota segundo trimestre"))
+media3 = float(input("digite a sua nota do terceiro trimestre"))
+
+media = (media1 + media2 + media3) / 3
+print(f"notas {media1:}, {media2:},p {media3:}")
+print(f"media aritimetica {media:}")
