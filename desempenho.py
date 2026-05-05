6ee707efb57eba9f538d5b89b598a7ae19878872
kaua <machadokaua546@gmail.com>
2026-05-05 11:56:50 -0300

    ativade

--- desempenho.py
+++ desempenho.py
@@ -3,7 +3,7 @@ def analisar_vendas(nome, lista_vendas, meta_mensal):
     bateu_meta = media >= meta_mensal
     status = "bateu" if bateu_meta else "não bateu"
     
-    return f"O vendedor {nome} teve média de {media:.2f} e {status} a meta."
+    return f"O vendedor {nome} teve média de {media} e {status} a meta."
 
 nome_vendedor = "Carlos"
 vendas = [1200, 1500, 1100, 1900]
